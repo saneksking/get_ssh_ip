98ecdea80bf31e0ed56ea4720d3bc42108438080
A.A. Suvorov <saneksking@gmail.com>
2024-06-13 14:07:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Get local IP
+
+---
+
+## Help:
+
+`pip install -r requirements.txt`
+`python app.py`
+
+### Install library:
+
+`pip install python-dotenv`
+
+<h4>After installing the library you need:</h4>
+1. Create file .env.
+2. Enter the bot token or other secret information into it. Example: `TOKEN = 'your tg token'`
---
+++ app.py
@@ -0,0 +1,23 @@
+import socket
+import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+    s.close()
+    login = os.getenv('LOGIN')
+    bot.reply_to(message, f'Your local IP-address: {local_ip};\n'
+                          f'Command: ssh {login}@{local_ip}')
+
+
+bot.polling()
